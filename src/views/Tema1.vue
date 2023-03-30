16f886999b1c2d3661327bf6cc9af8b68dd44fde
deividramirezarias <deivid.innovatec@gmail.com>
2023-03-30 16:04:19 -0500

    ajustes
    ajustes

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -282,7 +282,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure(data-aos="zoom-in-left")
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/NIWMNPAuUCo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
 
 
     .row.mt-3
